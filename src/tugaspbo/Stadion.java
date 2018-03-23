e8f78fd3279584402e45967e970c73a333560744
MuhammadHanafi182 <32392643+MuhammadHanafi182@users.noreply.github.com>
2018-03-24 00:00:05 +0700

    Membuat atribut dan behavior

--- src/tugaspbo/Stadion.java
+++ src/tugaspbo/Stadion.java
@@ -10,5 +10,18 @@ package tugaspbo;
  * @author Hanafi
  */
 public class Stadion {
+    String namastd;
+    String kapasitas;
+    String kota;
+    String negara;
+    String markas;
     
+       void cetakinfo(){
+        System.out.println("Nama Stadion        : "+namastd);
+        System.out.println("Kapasitas Stadion   : "+kapasitas);
+        System.out.println("Kota Asal Stadion   : "+kota);
+        System.out.println("Negara Asal Stadion : "+negara);
+        System.out.println("Markas Tim          : "+markas);
+        System.out.println("===================================");
+       }
 }
