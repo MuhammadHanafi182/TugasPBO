4db480ea129597ebffe74da3622b2066ac21f8cf
MuhammadHanafi182 <32392643+MuhammadHanafi182@users.noreply.github.com>
2018-03-24 00:30:42 +0700

    Mengisi nilai Atribut

--- src/tugaspbo/StadionAksi.java
+++ src/tugaspbo/StadionAksi.java
@@ -14,6 +14,25 @@ public class StadionAksi {
         Stadion satu= new Stadion ();
         Stadion dua= new Stadion ();
         Stadion tiga= new Stadion ();
+        
+        satu.namastd="Gelora Bung Tomo ";
+        satu.kapasitas="60000";
+        satu.kota="Surabaya";
+        satu.negara="Indonesia";
+        satu.markas="Persebaya Surabaya";
+        
+        dua.namastd="Si Jalak Harupat";
+        dua.kapasitas="40000";
+        dua.kota="Bandung";
+        dua.negara="Indonesia";
+        dua.markas="Persib Bandung dan Persikab Bandung";
+        
+        tiga.namastd="Gelora Bung Karno";
+        tiga.kapasitas="88306";
+        tiga.kota="Jakarta";
+        tiga.negara="Indonesia";
+        tiga.markas="Timnas Indonesia dan Persija Jakarta";
+        
     }
     
 }
